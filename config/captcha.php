c325c55f8a1dc7806e91c8e898dd1c294d7e4573
Nazeeh-Saifi <nazeeh.saifi@gmail.com>
2020-04-20 18:41:41 +0300

    changing the captcha configurations

--- config/captcha.php
+++ config/captcha.php
@@ -3,11 +3,12 @@
 return [
     'characters' => ['2', '3', '4', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j', 'm', 'n', 'p', 'q', 'r', 't', 'u', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'M', 'N', 'P', 'Q', 'R', 'T', 'U', 'X', 'Y', 'Z'],
     'default' => [
-        'length' => 9,
+        'length' => 4,
         'width' => 120,
-        'height' => 36,
-        'quality' => 90,
-        'math' => true,
+        'height' => 50,
+        'quality' => 100,
+        'fontColors' => ['#3490dc', '#e3342f', '#f6993f', '#ffed4a','#f57c00'],
+        'math' => false,
     ],
     'math' => [
         'length' => 9,
