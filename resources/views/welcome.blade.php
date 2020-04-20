e186eb271bee647b5e502bf0b8998433335c8a0c
Nazeeh-Saifi <nazeeh.saifi@gmail.com>
2020-04-20 11:26:26 +0300

    changing the welcome screen name from laravel to HealthHub

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    HealthHub
                 </div>
 
                 <div class="links">
