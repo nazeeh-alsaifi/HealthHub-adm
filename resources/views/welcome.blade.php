ea897c1b7a893e5c73796f3d8a836edd660563e7
mhdsulaiman <suliman.tanbari.st@gmail.com>
2020-04-22 12:31:42 +0300

    # ADDing loader class for the loading screen # ADDing Logo symbol using svg and animation # ADDing Horizontal menu with General fade in class # Removing the Home(login-register) button and leave it as a comment in the code

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -61,6 +61,171 @@
             .m-b-md {
                 margin-bottom: 30px;
             }
+
+<!-- fade in class -->
+
+            .fade-in{
+                animation: fade-in 5s;
+                animation-fill-mode: forwards;
+            }
+
+            @keyframes fade-in {
+                from {
+                    opacity: 0;
+                }
+                to{
+                    opacity: 1;
+                }
+            }
+
+<!-- loader class symbol -->
+            .loader {
+                position: fixed;
+                z-index: 99;
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 100%;
+                background: white;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                animation: fadeOut 2s;
+                animation-fill-mode: forwards;
+
+            }
+
+            .loader > img {
+                width: 100px;
+            }
+
+
+
+            @keyframes fadeOut {
+                100% {
+                    opacity: 0;
+                    visibility: hidden;
+                }
+            }
+
+<!-- logo HEALTHHUB -->
+            #logo {
+                animation: fill 0.5s ease forwards 3s;
+
+
+            }
+            #logo path:nth-child(1){
+                stroke-dasharray: 544.781982421875;
+                stroke-dashoffset: 544.781982421875;
+                animation: line-anim 2s ease forwards 0.3s;
+                animation: fill2 0.5s ease forwards 0.5s;
+
+            }
+            #logo path:nth-child(2){
+                stroke-dasharray: 525.2340087890625;
+                stroke-dashoffset: 525.2340087890625;
+                animation: line-anim 2s ease forwards 0.6s;
+            }
+            #logo path:nth-child(3){
+                stroke-dasharray:363.093994140625;
+                stroke-dashoffset: 363.093994140625;
+                animation: line-anim 2s ease forwards 0.9s;
+
+            }
+            #logo path:nth-child(4){
+                stroke-dasharray: 363.093994140625;
+                stroke-dashoffset: 363.093994140625;
+                animation: line-anim 2s ease forwards 1.2s;
+
+            }
+            #logo path:nth-child(5){
+                stroke-dasharray: 544.781982421875;
+                stroke-dashoffset: 544.781982421875;
+                animation: line-anim 2s ease forwards 1.5s;
+
+            }
+            #logo path:nth-child(6){
+                stroke-dasharray: 544.781982421875;
+                stroke-dashoffset: 544.781982421875;
+                animation: line-anim 2s ease forwards 1.8s;
+
+
+            }
+            #logo path:nth-child(7){
+                stroke-dasharray: 607.3242797851562;
+                stroke-dashoffset: 607.3242797851562;
+                animation: line-anim 2s ease forwards 2.1s;
+                animation: fill2 0.5s ease forwards 2s;
+
+
+            }
+            #logo path:nth-child(8){
+                stroke-dasharray: 607.3242797851562;
+                stroke-dashoffset: 607.3242797851562;
+                animation: line-anim 2s ease forwards 2.4s;
+            }
+            #logo path:nth-child(9){
+                stroke-dasharray: 607.3242797851562;
+                stroke-dashoffset: 607.3242797851562;
+                animation: line-anim 2s ease forwards 2.7s;
+
+            }
+
+            @keyframes line-anim {
+                to {
+                    stroke-dashoffset: 0;
+                }
+            }
+
+            @keyframes fill {
+                from{
+                    fill:transparent;
+                }
+                to{
+                    fill : black;
+                }
+            }
+
+            @keyframes fill2 {
+                from{
+                    fill:transparent;
+                }
+                to{
+                    fill :red;
+                }
+            }
+
+<!-- Horizontal Menu -->
+
+            ul {
+                list-style-type: none;
+                margin: 0;
+                padding: 0;
+                overflow: hidden;
+                background-color: #333;
+            }
+
+            li {
+                float: left;
+            }
+
+            li a {
+                display: block;
+                color: white;
+                text-align: center;
+                padding: 14px 16px;
+                text-decoration: none;
+            }
+
+            li a:hover:not(.active) {
+                background-color: #111;
+            }
+
+            .active {
+                background-color: #c51f1a;
+            }
+
+
         </style>
     </head>
     <body>
