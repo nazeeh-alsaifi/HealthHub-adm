96b58e93aeb2cc5366e113a1ac227b2fd3866882
mhdsulaiman <suliman.tanbari.st@gmail.com>
2020-04-25 16:26:09 +0300

    adding method for articles page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,4 +25,11 @@ class HomeController extends Controller
     {
         return view('home');
     }
+
+/* Controller for articles page*/
+
+    public function articles()
+    {
+        return view('articles');
+    }
 }
