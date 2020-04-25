b3efb03ec32b86f63a285797f717afaef5b778cf
Nazeeh-Saifi <nazeeh.saifi@gmail.com>
2020-04-25 17:30:31 +0300

    fixed some errors
    created new _header.scss to separate the styling

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,125 +1,140 @@
 @extends('layouts.app')
 
 @section('content')
-     <div class="fade-in"><img src="/images/heart.gif" width="150px" height="170px" style="position: relative;left: 45%" ></div>
-     <div class="fade-in"><div class="text-bar"><p>Your  Way  For  Better  Life.</p></div></div>
-    <div class="basic">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="fade-in"><img src="/images/heart.gif" width="150px" height="170px"
+                                      style="position: relative;left: 45%">
+            </div>
+            <div class="fade-in">
+                <div class="text-bar"><p>Your Way For Better Life.</p></div>
+            </div>
+            <div class="basic">
 
-        <article>
-                <div class="row">
-                    <div class="column">
-                        <div class="first">
-                        <div class="flip-card-container">
-                            <div class="flip-card">
-                                <div class="flip-card-front">
+                <article>
+                    <div class="row">
+                        <div class="column">
+                            <div class="first">
+                                <div class="flip-card-container">
+                                    <div class="flip-card">
+                                        <div class="flip-card-front">
 
-                                    <img src="/images/s-logo.png" alt="Albert Einstein">
+                                            <img src="/images/s-logo.png" alt="Albert Einstein">
+                                        </div>
+                                        <div class="flip-card-back">
+                                            <p>
+                                                "Two things are infinite: the universe and human stupidity; and I'm not
+                                                sure
+                                                about the universe."
+                                                <span>- Albert Einstein -</span>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div class="flip-card-back">
-                                    <p>
-                            "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe."
-                                        <span>- Albert Einstein -</span>
+                            </div>
+                        </div>
+
+                        <div class="column">
+                            <div class="second">
+                                <div class="flip-card-container">
+                                    <div class="flip-card">
+                                        <div class="flip-card-front">
+
+                                            <img src="/images/s-logo.png" alt="Albert Einstein">
+                                        </div>
+                                        <div class="flip-card-back">
+                                            <p>
+                                                "Two things are infinite: the universe and human stupidity; and I'm not
+                                                sure
+                                                about the universe."
+                                                <span>- Albert Einstein -</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="column">
+                            <div class="third">
+                                <div class="flip-card-container">
+                                    <div class="flip-card">
+                                        <div class="flip-card-front">
+
+                                            <img src="/images/s-logo.png" alt="Albert Einstein">
+                                        </div>
+                                        <div class="flip-card-back">
+                                            <p>
+                                                "Two things are infinite: the universe and human stupidity; and I'm not
+                                                sure
+                                                about the universe."
+                                                <span>- Albert Einstein -</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div class="column">
+                            <div class="fourth">
+                                <div class="flip-card-container">
+                                    <div class="flip-card">
+                                        <div class="flip-card-front">
+
+                                            <img src="/images/s-logo.png" alt="Albert Einstein">
+                                        </div>
+                                        <div class="flip-card-back">
+                                            <p>
+                                                "Two things are infinite: the universe and human stupidity; and I'm not
+                                                sure
+                                                about the universe."
+                                                <span>- Albert Einstein -</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="column">
+                            <div class="fifth">
+                                <div class="flip-card-container">
+                                    <div class="flip-card">
+                                        <div class="flip-card-front">
+
+                                            <img src="/images/s-logo.png" alt="Albert Einstein">
+                                        </div>
+                                        <div class="flip-card-back">
+                                            <p>
+                                                "Two things are infinite: the universe and human stupidity; and I'm not
+                                                sure
+                                                about the universe."
+                                                <span>- Albert Einstein -</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="column">
+                            <div class="sixth">
+                                <div class="flip-card-container">
+                                    <div class="flip-card">
+                                        <div class="flip-card-front">
+
+                                            <img src="/images/s-logo.png" alt="Albert Einstein">
+                                        </div>
+                                        <div class="flip-card-back">
+                                            <p>
+                                                "Two things are infinite: the universe and human stupidity; and I'm not
+                                                sure
+                                                about the universe."
+                                                <span>- Albert Einstein -</span>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    </div>
-
-                <div class="column">
-                    <div class="second">
-                    <div class="flip-card-container">
-                        <div class="flip-card">
-                            <div class="flip-card-front">
-
-                            <img src="/images/s-logo.png" alt="Albert Einstein">
-                            </div>
-                            <div class="flip-card-back">
-                            <p>
-                                "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe."
-                                <span>- Albert Einstein -</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                </div>
-                    <div class="column">
-                        <div class="third">
-                        <div class="flip-card-container">
-                            <div class="flip-card">
-                                <div class="flip-card-front">
-
-                            <img src="/images/s-logo.png" alt="Albert Einstein">
-                        </div>
-                        <div class="flip-card-back">
-                            <p>
-                                "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe."
-                                <span>- Albert Einstein -</span>
-                        </div>
-                    </div>
-                </div>
-                    </div>
-                    </div>
-
-
-                <div class="column">
-                    <div class="fourth">
-                    <div class="flip-card-container">
-                        <div class="flip-card">
-                            <div class="flip-card-front">
-
-                                <img src="/images/s-logo.png" alt="Albert Einstein">
-                            </div>
-                            <div class="flip-card-back">
-                                <p>
-                                    "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe."
-                                    <span>- Albert Einstein -</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                </div>
-
-                <div class="column">
-                    <div class="fifth">
-                    <div class="flip-card-container">
-                        <div class="flip-card">
-                            <div class="flip-card-front">
-
-                                <img src="/images/s-logo.png" alt="Albert Einstein">
-                            </div>
-                            <div class="flip-card-back">
-                                <p>
-                                    "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe."
-                                    <span>- Albert Einstein -</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                </div>
-
-                <div class="column">
-                    <div class="sixth">
-                    <div class="flip-card-container">
-                        <div class="flip-card">
-                            <div class="flip-card-front">
-
-                                <img src="/images/s-logo.png" alt="Albert Einstein">
-                            </div>
-                            <div class="flip-card-back">
-                                <p>
-                                    "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe."
-                                    <span>- Albert Einstein -</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                </div>
-                </div>
-        </article>
+                </article>
+            </div>
+        </div>
     </div>
- @endsection
-        <!-- End Footer -->
-        {{--end app--}}
-    </body>
-</html>
-
+@endsection
