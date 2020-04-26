9d8517a791618e5e5007885ad5c6ac09249f7086
Nazeeh-Saifi <nazeeh.saifi@gmail.com>
2020-04-26 12:52:30 +0300

    fixing the logout styling in navigation bar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -63,35 +63,36 @@
                     <li class="nav-item"><a class="nav-link" href="#portfolio">Portfolio</a></li>
                     <li class="nav-item"><a class="nav-link" href="#pricing">Pricing</a></li>
                     <li class="nav-item"><a class="nav-link" href="#team">Team</a></li>
-                    <li class="nav-item drop-down">
-                        @guest
+                    @guest
+                        <li class="nav-item drop-down">
                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                 Register/Login <span class="caret"></span>
-                            </a>                            <ul>
+                            </a>
+                            <ul>
                                 <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
                                 @if (Route::has('register'))
                                     <li><a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a></li>
                                 @endif
                             </ul>
                     @else
-                        <li class="nav-item dropdown">
+                        <li class="nav-item drop-down">
                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                 {{ Auth::user()->name }} <span class="caret"></span>
                             </a>
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                   onclick="event.preventDefault();
+                            <ul>
+                                <li><a class="nav-link" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                                      style="display: none;">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
+                                        {{ __('Logout') }}
+                                    </a>
+                                </li>
+                            </ul>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                  style="display: none;">
+                                @csrf
+                            </form>
                     @endguest
                 </ul>
             </nav>
