f80563d4a484ffcc43feaf3aa4ceabc74671f385
Nazeeh-Saifi <nazeeh.saifi@gmail.com>
2020-04-27 18:21:05 +0300

    1- adding a title to the schema (the difference between the title and the description) title is the title of the article while description is a short description of the content of the article and can be used in the home view 2- adding a string this will be filled with the source of the image (we do not store images in databases)

--- database/migrations/2020_04_27_145853_create_articles_table.php
+++ database/migrations/2020_04_27_145853_create_articles_table.php
@@ -16,9 +16,12 @@ class CreateArticlesTable extends Migration
         Schema::create('articles', function (Blueprint $table) {
             $table->id('article_id');
             $table->timestamps('created_at');
+            $table->string('title');
             $table->text('description');
             $table->longText('content');
+            $table->string('image');
             $table->bigInteger('views');
+
         });
     }
 
