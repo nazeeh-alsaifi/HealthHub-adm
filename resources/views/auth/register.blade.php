0d423b86428811fcab89de8e5aa6982f21341d65
Nazeeh-Saifi <nazeeh.saifi@gmail.com>
2020-04-29 22:01:37 +0300

    - fixed username in register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -24,21 +24,18 @@
                                 @enderror
                             </div>
                         </div>
+
                         <div class="form-group row">
-                            <label for="username" class="col-md-4 col-form-label text-md-right">
-                                {{ __('Username') }}
-                            </label>
+                            <label for="username" class="col-md-4 col-form-label text-md-right">{{ __('UserName') }}</label>
 
                             <div class="col-md-6">
-                                <input id="username" type="text"
-                                       class="form-control{{ $errors->has('username') ? ' is-invalid' : '' }}"
-                                       name="username" value="{{ old('username') }}" required>
+                                <input id="username" type="text" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" required autocomplete="username" autofocus>
 
-                                @if ($errors->has('username'))
-                                    <span class="invalid-feedback">
-                <strong>{{ $errors->first('username') }}</strong>
-            </span>
-                                @endif
+                                @error('username')
+                                <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </div>
                         </div>
 
