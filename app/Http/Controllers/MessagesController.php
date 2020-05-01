e9e751738bfac73b460f1a29a0c27519c6a57a1e
Nazeeh-Saifi <nazeeh.saifi@gmail.com>
2020-05-01 13:18:06 +0300

    - adding the middleware auth to make only authenticated user able to send a message to admin -implementing the create method which return the create message view -creating the store method and (die and dump) the request

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -6,5 +6,31 @@ use Illuminate\Http\Request;
 
 class MessagesController extends Controller
 {
-    //
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+
+    public function create(){
+        return view('messages.create');
+    }
+
+    public function store(Request $request){
+        dd($request);
+//        $data = $request->validate([
+//            'content' => ['required','string','min:10'],
+//            'captcha' => ['required','captcha'],
+//        ]);
+//        $user= auth()->user();
+//        $user->consults()->create([
+//            'content' => $data['content'],
+//            'sent_by' => $user->username,
+//            'received_by' => 'admin1'
+//        ]);
+//        return redirect('/home');
+
+
+    }
 }
