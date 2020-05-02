663f4637711b0d41c629f9cb6f84a446df6761a4
mhdsulaiman <suliman.tanbari.st@gmail.com>
2020-05-03 00:01:31 +0300

    adding the consultation in to the users home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -173,4 +173,92 @@
             </div>
         </div>
     </div>
+    @if(!(Auth::user() == null))
+        <div id="consult">
+            <div id="consults-create" class="container" style="padding: 12vh;">
+                <div class="row justify-content-center">
+                    <div class="py-4 col-md-8">
+                        <div class="card">
+                            <div class="card-header">{{ __('Write Your Consult') }}</div>
+
+                            <div class="card-body">
+                                <form method="POST" action="{{ url('/message')}}" enctype="multipart/form-data">
+                                    @csrf
+
+                                    <div class="form-group row">
+                                        <label for="subject"
+                                               class="col-md-4 col-form-label text-md-right">{{ __('Subject') }}</label>
+
+                                        <div class="col-md-6">
+                                            <input id="subject" type="text"
+                                                   class="form-control @error('subject') is-invalid @enderror"
+                                                   name="subject" value="{{ old('subject') }}" required
+                                                   autocomplete="subject" autofocus>
+
+                                            @error('subject')
+                                            <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                            @enderror
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group row">
+                                        <label for="body"
+                                               class="col-md-4 col-form-label text-md-right">{{ __('Content') }}</label>
+
+                                        <div class="col-md-6">
+                                                    <textarea id="body"
+                                                              class="form-control @error('body') is-invalid @enderror"
+                                                              name="body" required></textarea>
+
+                                            @error('body')
+                                            <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                            @enderror
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group row">
+                                        <label for="captcha"
+                                               class="col-md-4 col-form-label text-md-right">{{ __('captcha') }}</label>
+
+                                        <div class="col-md-6">
+                                            <div class="captcha">
+                                                <span>{!! captcha_img() !!}</span>
+                                                <button type="button" class="btn btn-success btn-refresh">
+                                                    Refresh
+                                                </button>
+                                            </div>
+                                            <input id="captcha" type="text"
+                                                   class="form-control mt-2 @error('captcha') is-invalid @enderror"
+                                                   name="captcha"
+                                                   placeholder="Enter Captcha" required>
+
+                                            @error('captcha')
+                                            <span class="invalid-feedback" role="alert">
+                                        <strong>Wrong Captcha</strong>
+                                    </span>
+                                            @enderror
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group row mb-0">
+                                        <div class="col-md-6 offset-md-4">
+                                            <button type="submit" class="btn btn-primary">
+                                                {{ __('Submit') }}
+                                            </button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endif
 @endsection
+
+
