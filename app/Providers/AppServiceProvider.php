0497ddd5603158f3f9026f647a7f61a71a9a4041
mhdsulaiman <suliman.tanbari.st@gmail.com>
2020-05-05 13:35:59 +0300

    adding some functionality to some features

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,18 +4,11 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use BeyondCode\Mailbox\InboundEmail;
+use BeyondCode\Mailbox\Facades\Mailbox;
+
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
-
     /**
      * Bootstrap any application services.
      *
@@ -23,6 +16,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+
     }
 }
