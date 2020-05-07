eecf001baeaad8f7d503865696f894ae0f9800cf
mhdsulaiman <suliman.tanbari.st@gmail.com>
2020-05-07 16:39:22 +0300

    adding an admin controller for admin activities

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -7,11 +7,20 @@ use Illuminate\Http\Request;
 use App\Article;
 
 
-class ArticlesController extends Controller
+class AdminController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('admin');
+    }
+
+    public function admin(){
+
+    $most_viewed=Article::orderBy('views','DESC')->get()->take(3);
+
+    $articles = Article::orderBy('created_at','DESC')->get();
+
+    return view('admin',['most_viewed' => $most_viewed,'articles' =>$articles]);
     }
 
     public function create()
@@ -28,29 +37,23 @@ class ArticlesController extends Controller
             'title' => 'required',
             'description' => 'required',
             'content' => 'required']);
+//dd(Article::orderBy('article_id', 'desc')->increment('article_id'));
+
 
         $imagePath = request('image')->store('uploads', 'public');
         Article::create([
+
             'image' => $imagePath,
             'title' => $data['title'],
             'description' => $data['description'],
-            'content' => $data['content']]);
-
+            'content' => $data['content']
+            ]);
+        Article::orderBy('article_id', 'desc')->increment('article_id');
         // $image= image::make(public_path("storage/{imagePath}"))->fit(1200,500);
         // $image->save();
         return redirect('/home/admin');
 
 
-    }
-
-    public function show($article_id)
-    {
-
-        Article::where('article_id', '=', $article_id)->increment('views');
-
-        $articleInfo = Article::where('article_id', '=', $article_id)->first();
-
-        return view('articles.show', compact('articleInfo'));
     }
 
     public function edit($article_id)
@@ -86,9 +89,10 @@ class ArticlesController extends Controller
     public function delete($article_id)
     {
 
+
         Article::where('article_id', '=', $article_id)->delete();
 
-        //dd(Article::decrement('article_id'));
+        Article::where('article_id','>',$article_id)->decrement('article_id');
 
         return redirect('/home/admin');
     }
