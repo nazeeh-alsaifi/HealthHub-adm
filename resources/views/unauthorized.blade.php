6ccb0f3368c3c6541113ffa325a43591957ad92e
mhdsulaiman <suliman.tanbari.st@gmail.com>
2020-05-07 16:20:04 +0300

    adding a page for unauthorized accesses

---
+++ resources/views/unauthorized.blade.php
@@ -0,0 +1,4 @@
+<div class="title m-b-md" style="padding-top: 50vh">
+    <center><h1>Sorry! there is no page found.  &#128516;</h1>
+        <a href="{{route('member', $username ?? '')}}"><p>You can try to go back to home page</p></a></center>
+</div>
