c5b0d01d1fc33a5d0462e631a5111911f2738f19
mhdsulaiman <suliman.tanbari.st@gmail.com>
2020-05-07 16:29:35 +0300

    deleting the fixed articles and replace them with those in the articles table

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,162 +2,111 @@
 
 @section('content')
     <div id="most_viewed">
-        <section class="home-slideshow">
-            <div class="slider">
-                <div class="slide active-slide" style="background-image: url('/images/Health-1.jpg')">
-                    <div class="text-container">
-                        <div class="caption">
-                            <h1>1. Heart diseases</h1>
-                            <p>The most known heart diseases in Uk</p>
-                            <a href="/articles">Find More Here</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="slide" style="background-image: url('/images/Health-2.jpg')">
-                    <div class="text-container">
-                        <div class="caption">
-                            <h1>2. Heart attacks</h1>
-                            <p>Did you know that 30% of elders die because of it</p>
-                            <a href="/articles">Find More Here</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="slide" style="background-image: url('/images/Health-3.jpg')">
-                    <div class="text-container">
-                        <div class="caption">
-                            <h1>3. Improving your Health</h1>
-                            <p>you can find out more about this topic in the link below</p>
-                            <a href="/articles">Find More Here</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="slide" style="background-image: url('/images/Health-4.jpg')">
-                    <div class="text-container">
-                        <div class="caption">
-                            <h1>4. How to improve your health</h1>
-                            <p>you can find out more about this topic in the link below</p>
-                            <a href="/articles">Find More Here</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
+        @if($most_viewed->isEmpty())
+            <div id="home" class="container" style="padding: 30px">
+                <div class="row justify-content-center">
+                    <div class="py-4 col-md-8">
 
-            <div class="controls">
-                <div class="prev"><</div>
-                <div class="next">></div>
-            </div>
-            <div class="indicator">
+                        <div class="card">
+                            <div class="card-header">Most Viewed</div>
+                            <div class="row">
 
+                                <div class="col-lg-12 col-sm-6 mb-2 mt-2" style="text-align: center">
+                                    <h3>Empty</h3>
+                                    <p>you didn't add any article.</p></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </section>
+        @else
+            {{$i=1}}
+
+            <section class="home-slideshow">
+                <div class="slider">
+                    @foreach($most_viewed as $article)
+
+
+                        <div class="slide active-slide" style="background-image: url('/storage/{{$article->image}}')">
+                            <div class="text-container">
+                                <div class="caption">
+                                    <h1>{{$i}}. {{$article->title}}</h1>
+                                    <p>{{$article->description}}</p>
+                                    <a href="{{route('article_show',$article->article_id)}}">Find More Here</a>
+                                </div>
+                            </div>
+                        </div>
+
+                        {{$i++}}
+                    @endforeach
+                </div>
+                <div class="controls">
+                    <div class="prev"><</div>
+                    <div class="next">></div>
+                </div>
+
+
+                <div class="indicator">
+
+                </div>
+            </section>
     </div>
-    <div id="home" class="container">
-        <div id="articles" style="padding: 30px">
-            <div class="row justify-content-center">
-                <div class="py-4 col-md-8">
 
-                    <div class="card">
-                        <div class="card-header">Articles</div>
+    @endif
+    </div>
+    <div id="articles" style="padding: 30px">
+        <div class="row justify-content-center">
+            <div class="py-4  col-md-8">
 
-                        <div class="row">
-                            <div class="col-lg-4 col-sm-6 mb-2 mt-2">
-                                <div class="smaller-card card h-100">
-                                    <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-                                    <div class="card-body">
-                                        <h4 class="card-title">
-                                            <a href="{{route('article_show',$username ?? '')}}">Article One</a>
-                                        </h4>
-                                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                                            Amet
-                                            numquam aspernatur eum quasi sapiente nesciunt? Voluptatibus sit, repellat
-                                            sequi
-                                            itaque deserunt, dolores in, nesciunt, illum tempora ex quae? Nihil,
-                                            dolorem!</p>
+                <div class="card">
+
+                    <div class="card-header pt-2">Articles</div>
+
+                    <div class="row">
+
+                        @if($articles->isEmpty())
+                            <div class="col-lg-12 col-sm-6 mb-2 mt-2" style="text-align: center">
+                                <h3>Empty</h3>
+                                <p>you didn't add any article.</p></div>
+                        @else
+                            @foreach($articles as $article)
+                                <div class="col-lg-4 col-sm-6 mb-2 mt-2">
+
+                                    <div class="smaller-card card h-100" style="text-align: center">
+                                        <a href="{{route('article_show',$article->article_id)}}"><img
+                                                class="card-img-top"
+                                                src="/storage/{{$article->image}}"
+                                                alt=""></a>
+                                        <div class="card-body">
+                                            <h4 class="card-title">
+                                                <a href="{{route('article_show',$article->article_id)}}">{{$article->title}}</a>
+                                            </h4>
+                                            <p class="card-text">{{$article->description}}</p>
+
+                                            <small><b>Viewed by: {{$article->views}} &#128065;</b>
+
+                                                <p>published at: {{$article->created_at}}</p></small>
+
+                                        </div>
+
+
                                     </div>
                                 </div>
-                            </div>
-                            <div class="col-lg-4 col-sm-6 mb-2 mt-2">
-                                <div class="smaller-card card h-100">
-                                    <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-                                    <div class="card-body">
-                                        <h4 class="card-title">
-                                            <a href="{{route('article_show',$username ?? '')}}">Article Two</a>
-                                        </h4>
-                                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                            Nam
-                                            viverra euismod odio, gravida pellentesque urna varius vitae.</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-lg-4 col-sm-6 mb-2 mt-2">
-                                <div class="smaller-card card h-100">
-                                    <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-                                    <div class="card-body">
-                                        <h4 class="card-title">
-                                            <a href="{{route('article_show',$username ?? '')}}">Article Three</a>
-                                        </h4>
-                                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                                            Quos
-                                            quisquam, error quod sed cumque, odio distinctio velit nostrum temporibus
-                                            necessitatibus et facere atque iure perspiciatis mollitia recusandae vero
-                                            vel
-                                            quam!</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-lg-4 col-sm-6 mb-2 mt-2">
-                                <div class="smaller-card card h-100">
-                                    <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-                                    <div class="card-body">
-                                        <h4 class="card-title">
-                                            <a href="{{route('article_show',$username ?? '')}}">Article Four</a>
-                                        </h4>
-                                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                            Nam
-                                            viverra euismod odio, gravida pellentesque urna varius vitae.</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-lg-4 col-sm-6 mb-2 mt-2">
-                                <div class="smaller-card card h-100">
-                                    <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-                                    <div class="card-body">
-                                        <h4 class="card-title">
-                                            <a href="{{route('article_show',$username ?? '')}}">Article Five</a>
-                                        </h4>
-                                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                            Nam
-                                            viverra euismod odio, gravida pellentesque urna varius vitae.</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-lg-4 col-sm-6 mb-2 mt-2">
-                                <div class="smaller-card card h-100">
-                                    <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-                                    <div class="card-body">
-                                        <h4 class="card-title">
-                                            <a href="{{route('article_show',$username ?? '')}}">Article Six</a>
-                                        </h4>
-                                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                                            Itaque earum nostrum suscipit ducimus nihil provident, perferendis rem illo,
-                                            voluptate atque, sit eius in voluptates, nemo repellat fugiat excepturi!
-                                            Nemo,
-                                            esse.</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <!-- /.row -->
+                            @endforeach
+                        @endif
                     </div>
                 </div>
             </div>
+            <!-- /.row -->
         </div>
-        <div id="services" style="padding: 30px">
-            <div class="row justify-content-center">
-                <div class="py-4 col-md-8">
-                    <div class="card">
-                        <div class="card-header">services</div>
-                    </div>
+    </div>
+    </div>
+
+    <div id="services" style="padding: 30px">
+        <div class="row justify-content-center">
+            <div class="py-4 col-md-8">
+                <div class="card">
+                    <div class="card-header">services</div>
                 </div>
             </div>
         </div>
@@ -207,7 +156,7 @@
                                     <div class="col-md-6">
                                                     <textarea id="body"
                                                               class="form-control @error('body') is-invalid @enderror"
-                                                              name="body" required></textarea>
+                                                              name="body" required>{{old('body')}}</textarea>
 
                                         @error('body')
                                         <span class="invalid-feedback" role="alert">
@@ -254,6 +203,7 @@
                 </div>
             </div>
         </div>
+
         <div id="admin-replies" style="padding: 30px">
             <div class="row justify-content-center">
                 <div class="py-4 col-md-8">
@@ -283,7 +233,8 @@
                                                 <td>{{$message->created_at}}</td>
                                                 @if((Auth::User()->receive->where('reply_on',$message->id)->first()) != null)
                                                     <td><a id="reply-link" data-toggle="collapse"
-                                                           href="#{{'reply'. $message->id}}">View Reply</a></td>
+                                                           href="#{{'reply'. $message->id}}">View Admin's Reply</a>
+                                                    </td>
                                                 @endif
                                             </tr>
                                             <tr class="hide-table-padding">
