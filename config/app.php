29dfdc064d95e5426052bdc7d7408c117aa3a219
mhdsulaiman <suliman.tanbari.st@gmail.com>
2020-05-08 13:31:43 +0300

    also packages for Intervention library

--- config/app.php
+++ config/app.php
@@ -161,7 +161,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-
+        Intervention\Image\ImageServiceProvider::class,
         /*
          * Package Service Providers...
          */
@@ -227,8 +227,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Captcha' => mews\Captcha\Facades\Captcha::class
-
+        'Captcha' => mews\Captcha\Facades\Captcha::class,
+        'Image' => Intervention\Image\Facades\Image::class
     ],
 
 ];
