1817ccbf9defe27c7f2696dda6900507529140ce
mhdsulaiman <suliman.tanbari.st@gmail.com>
2020-05-10 13:50:09 +0300

    adding sweetalert2 script

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
 {{--    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>--}}
 {{--    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>--}}
 {{--    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>--}}
@@ -220,6 +221,5 @@
     <!-- End Footer -->
 </div>
 {{--end app--}}
-@stack('script')
 </body>
 </html>
