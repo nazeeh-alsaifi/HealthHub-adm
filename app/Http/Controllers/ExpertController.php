5df1c6c76cfedccf7e4f5dc31dd2152fafd162fe
Nazeeh-Saifi <nazeeh.saifi@gmail.com>
2020-05-30 22:16:40 +0300

    - finished bayes integration and result viewing

--- app/Http/Controllers/ExpertController.php
+++ app/Http/Controllers/ExpertController.php
@@ -1,21 +1,102 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\expert_alg\bayes;
 use Illuminate\Http\Request;
+
 class ExpertController extends Controller
 {
+    private $bayes;
+
     function __construct()
     {
-        $bayes = new bayes(public_path('bayes_training.csv'));
+        $this->bayes = new bayes(public_path('bayes_training.csv'));
         $this->middleware('auth');
     }
 
-    function userCreate(){
+    function userCreate()
+    {
         return view('expert_system.create');
     }
 
-    function predict(){
-        dd(request()->all());
+    function predict()
+    {
+        $validated_data = request()->validate([
+            'discrete_age' => ['required', 'numeric', 'min:0', 'not_in:0'],
+            'chest_pain_type' => ['required', 'string'],
+            'discrete_rest_blood' => ['required', 'numeric', 'min:0', 'not_in:0'],
+            'blood_sugar' => ['required', 'string'],
+            'rest_electro' => ['required', 'string'],
+            'discrete_max_heart' => ['required', 'numeric', 'min:0', 'not_in:0'],
+            'exercice_angina' => ['required', 'string'],
+            'expert_algorithm' => ['required', 'string'],
+        ]);
+        $age = $validated_data['discrete_age'];
+        if ($age < 35) {
+            $validated_data['discrete_age'] = 'groupA1';
+        } elseif ($age >= 35 && $age < 45) {
+            $validated_data['discrete_age'] = 'groupA2';
+
+        } elseif ($age >= 45 && $age < 55) {
+            $validated_data['discrete_age'] = 'groupA3';
+
+        } elseif ($age >= 55 && $age < 65) {
+            $validated_data['discrete_age'] = 'groupA4';
+
+        } elseif ($age >= 65) {
+            $validated_data['discrete_age'] = 'groupA5';
+        }
+
+        $rest_blood = $validated_data['discrete_rest_blood'];
+        if ($rest_blood < 110) {
+            $validated_data['discrete_rest_blood'] = 'groupC1';
+
+        } elseif ($rest_blood >= 110 && $rest_blood < 130) {
+            $validated_data['discrete_rest_blood'] = 'groupC2';
+
+        } elseif ($rest_blood >= 130 && $rest_blood < 150) {
+            $validated_data['discrete_rest_blood'] = 'groupC3';
+
+        } elseif ($rest_blood >= 150 && $rest_blood < 170) {
+            $validated_data['discrete_rest_blood'] = 'groupC4';
+
+        } elseif ($rest_blood >= 170 && $rest_blood < 190) {
+            $validated_data['discrete_rest_blood'] = 'groupC5';
+        } elseif ($rest_blood >= 190) {
+            $validated_data['discrete_rest_blood'] = 'groupC6';
+        }
+
+        $max_heart = $validated_data['discrete_max_heart'];
+        if ($max_heart < 100) {
+            $validated_data['discrete_max_heart'] = 'groupF1';
+        } elseif ($max_heart >= 100 && $max_heart < 120) {
+            $validated_data['discrete_max_heart'] = 'groupF2';
+
+        } elseif ($max_heart >= 120 && $max_heart < 140) {
+            $validated_data['discrete_max_heart'] = 'groupF3';
+
+        } elseif ($max_heart >= 140 && $max_heart < 160) {
+            $validated_data['discrete_max_heart'] = 'groupF4';
+
+        } elseif ($max_heart >= 160 && $max_heart < 180) {
+            $validated_data['discrete_max_heart'] = 'groupF5';
+
+        } elseif ($max_heart >= 180) {
+            $validated_data['discrete_max_heart'] = 'groupF6';
+
+        }
+        $desired_alg = $validated_data['expert_algorithm'];
+        unset($validated_data['expert_algorithm']);
+        if ($desired_alg == 'bayes') {
+            $result = $this->bayes->predictUsingArray($validated_data);
+           // dd($result);
+            return view('expert_system.create', ['result' => $result]);
+        } else {
+            dd($validated_data);
+
+        }
+
+
     }
 }
--- resources/views/expert_system/create.blade.php
+++ resources/views/expert_system/create.blade.php
@@ -8,14 +8,16 @@
                     <div class="card-header input-label text-center">Fill The Application :</div>
 
                     <div class="card-body">
-                        <form method="post" action="{{route('article_store')}}" enctype="multipart/form-data">
+                        <form method="post" action="{{route('expert_predict')}}" enctype="multipart/form-data">
                             @csrf
                             <div class="expert-row form-group row">
                                 <label for="age"
                                        class="input-label col-md-4 col-form-label text-md-right">{{ __('Age:') }}</label>
 
                                 <div class="col-md-6">
-                                    <input id="age" type="text" class="form-control @error('age') is-invalid @enderror" name="discrete_age" value="{{ old('age') }}" required autocomplete="age" autofocus>
+                                    <input id="age" type="text" class="form-control @error('age') is-invalid @enderror"
+                                           name="discrete_age" value="{{ old('age') }}" required autocomplete="age"
+                                           autofocus>
 
                                     @error('age')
                                     <span class="invalid-feedback" role="alert">
@@ -32,7 +34,9 @@
                                        class="col-sm-2 col-form-label text-md-right">{{ __('Asympt') }}</label>
 
                                 <div class="expert-radio-button">
-                                    <input id="chest_pain_type" type="radio" class="form-control @error('chest_pain_type') is-invalid @enderror" name="chest_pain_type" value="asympt" checked required  autofocus>
+                                    <input id="chest_pain_type" type="radio"
+                                           class="form-control @error('chest_pain_type') is-invalid @enderror"
+                                           name="chest_pain_type" value="asympt" checked required autofocus>
 
                                     @error('chest_pain_type')
                                     <span class="invalid-feedback" role="alert">
@@ -45,7 +49,9 @@
                                        class="col-sm-2 col-form-label text-md-right">{{ __('Atyp_angina') }}</label>
 
                                 <div class="expert-radio-button">
-                                    <input id="chest_pain_type" type="radio" class="form-control @error('chest_pain_type') is-invalid @enderror" name="chest_pain_type" value="atyp_angina" required  autofocus>
+                                    <input id="chest_pain_type" type="radio"
+                                           class="form-control @error('chest_pain_type') is-invalid @enderror"
+                                           name="chest_pain_type" value="atyp_angina" required autofocus>
 
                                     @error('chest_pain_type')
                                     <span class="invalid-feedback" role="alert">
@@ -58,7 +64,9 @@
                                        class="col-sm-2 col-form-label text-md-right">{{ __('Non_anginal') }}</label>
 
                                 <div class="expert-radio-button">
-                                    <input id="chest_pain_type" type="radio" class="form-control @error('chest_pain_type') is-invalid @enderror" name="chest_pain_type" value="non_anginal" required  autofocus>
+                                    <input id="chest_pain_type" type="radio"
+                                           class="form-control @error('chest_pain_type') is-invalid @enderror"
+                                           name="chest_pain_type" value="non_anginal" required autofocus>
 
                                     @error('chest_pain_type')
                                     <span class="invalid-feedback" role="alert">
@@ -73,7 +81,10 @@
                                        class="input-label col-md-4 col-form-label text-md-right">{{ __('rest_blood') }}</label>
 
                                 <div class="col-md-6">
-                                    <input id="discrete_rest_blood" type="text" class="form-control @error('discrete_rest_blood') is-invalid @enderror" name="discrete_discrete_rest_blood" value="{{ old('discrete_rest_blood') }}" required autocomplete="discrete_rest_blood" autofocus>
+                                    <input id="discrete_rest_blood" type="text"
+                                           class="form-control @error('discrete_rest_blood') is-invalid @enderror"
+                                           name="discrete_rest_blood" value="{{ old('discrete_rest_blood') }}" required
+                                           autocomplete="discrete_rest_blood" autofocus>
 
                                     @error('discrete_rest_blood')
                                     <span class="invalid-feedback" role="alert">
@@ -88,12 +99,13 @@
                                        class="input-label col-md-4 col-lg-4 col-form-label text-md-right">{{ __('Blood Sugar:') }}</label>
 
 
-
                                 <label for="blood_sugar"
                                        class="col-sm-2  col-form-label  text-md-right">{{ __('Yes') }}</label>
 
                                 <div class="expert-radio-button">
-                                    <input id="blood_sugar" type="radio" class="form-control @error('blood_sugar') is-invalid @enderror" name="blood_sugar" value="yes" checked required  autofocus>
+                                    <input id="blood_sugar" type="radio"
+                                           class="form-control @error('blood_sugar') is-invalid @enderror"
+                                           name="blood_sugar" value="yes" checked required autofocus>
 
                                     @error('blood_sugar')
                                     <span class="invalid-feedback" role="alert">
@@ -107,7 +119,9 @@
                                        class="col-sm-2 col-form-label text-md-right">{{ __('No') }}</label>
 
                                 <div class="expert-radio-button">
-                                    <input id="blood_sugar" type="radio" class="form-control @error('blood_sugar') is-invalid @enderror" name="blood_sugar" value="no" required  autofocus>
+                                    <input id="blood_sugar" type="radio"
+                                           class="form-control @error('blood_sugar') is-invalid @enderror"
+                                           name="blood_sugar" value="no" required autofocus>
 
                                     @error('blood_sugar')
                                     <span class="invalid-feedback" role="alert">
@@ -122,12 +136,13 @@
                                        class="input-label col-md-4 col-lg-4 col-form-label text-md-right">{{ __('Rest Electro:') }}</label>
 
 
-
                                 <label for="rest_electro"
                                        class="col-sm-2  col-form-label  text-md-right">{{ __('Normal') }}</label>
 
                                 <div class="expert-radio-button">
-                                    <input id="rest_electro" type="radio" class="form-control @error('rest_electro') is-invalid @enderror" name="rest_electro" value="normal" checked required  autofocus>
+                                    <input id="rest_electro" type="radio"
+                                           class="form-control @error('rest_electro') is-invalid @enderror"
+                                           name="rest_electro" value="normal" checked required autofocus>
 
                                     @error('rest_electro')
                                     <span class="invalid-feedback" role="alert">
@@ -141,7 +156,9 @@
                                        class="col-sm-2 col-form-label text-md-right">{{ __('Left_vent_hyper') }}</label>
 
                                 <div class="expert-radio-button">
-                                    <input id="rest_electro" type="radio" class="form-control @error('rest_electro') is-invalid @enderror" name="rest_electro" value="left_vent_hyper" required  autofocus>
+                                    <input id="rest_electro" type="radio"
+                                           class="form-control @error('rest_electro') is-invalid @enderror"
+                                           name="rest_electro" value="left_vent_hyper" required autofocus>
 
                                     @error('rest_electro')
                                     <span class="invalid-feedback" role="alert">
@@ -154,7 +171,9 @@
                                        class="col-sm-2 col-form-label text-md-right">{{ __('St_t_wave_abnormality') }}</label>
 
                                 <div class="expert-radio-button">
-                                    <input id="rest_electro" type="radio" class="form-control @error('rest_electro') is-invalid @enderror" name="rest_electro" value="st_t_wave_abnormality" required  autofocus>
+                                    <input id="rest_electro" type="radio"
+                                           class="form-control @error('rest_electro') is-invalid @enderror"
+                                           name="rest_electro" value="st_t_wave_abnormality" required autofocus>
 
                                     @error('rest_electro')
                                     <span class="invalid-feedback" role="alert">
@@ -169,7 +188,10 @@
                                        class="input-label col-md-4 col-form-label text-md-right">{{ __('Max Heart Rate:') }}</label>
 
                                 <div class="col-md-6">
-                                    <input id="discrete_max_heart" type="text" class="form-control @error('discrete_max_heart') is-invalid @enderror" name="discrete_max_heart" value="{{ old('discrete_max_heart') }}" required autocomplete="discrete_max_heart" autofocus>
+                                    <input id="discrete_max_heart" type="text"
+                                           class="form-control @error('discrete_max_heart') is-invalid @enderror"
+                                           name="discrete_max_heart" value="{{ old('discrete_max_heart') }}" required
+                                           autocomplete="discrete_max_heart" autofocus>
 
                                     @error('discrete_max_heart')
                                     <span class="invalid-feedback" role="alert">
@@ -184,12 +206,13 @@
                                        class="input-label col-md-4 col-form-label text-md-right">{{ __('Exercice Angina:') }}</label>
 
 
-
                                 <label for="exercice_angina"
                                        class="col-sm-2  col-form-label  text-md-right">{{ __('Yes') }}</label>
 
                                 <div class="expert-radio-button">
-                                    <input id="exercice_angina" type="radio" class="form-control @error('exercice_angina') is-invalid @enderror" name="exercice_angina" value="yes" checked required  autofocus>
+                                    <input id="exercice_angina" type="radio"
+                                           class="form-control @error('exercice_angina') is-invalid @enderror"
+                                           name="exercice_angina" value="yes" checked required autofocus>
 
                                     @error('exercice_angina')
                                     <span class="invalid-feedback" role="alert">
@@ -203,7 +226,9 @@
                                        class="col-sm-2 col-form-label text-md-right">{{ __('No') }}</label>
 
                                 <div class="expert-radio-button">
-                                    <input id="exercice_angina" type="radio" class="form-control @error('exercice_angina') is-invalid @enderror" name="exercice_angina" value="no" required  autofocus>
+                                    <input id="exercice_angina" type="radio"
+                                           class="form-control @error('exercice_angina') is-invalid @enderror"
+                                           name="exercice_angina" value="no" required autofocus>
 
                                     @error('exercice_angina')
                                     <span class="invalid-feedback" role="alert">
@@ -214,18 +239,18 @@
                             </div>
 
 
-
                             <div class="expert-row form-group row">
                                 <label for="expert-algorithm"
                                        class="input-label col-md-4 col-form-label text-md-right">{{ __('Choose Algorithm:') }}</label>
 
 
-
                                 <label for="expert-algorithm"
-                                       class="col-sm-2  col-form-label  text-md-right">{{ __('ID3') }}</label>
+                                       class="col-sm-2  col-form-label  text-md-right">{{ __('id3') }}</label>
 
                                 <div class="expert-radio-button">
-                                    <input id="expert-algorithm" type="radio" class="form-control @error('expert-algorithm') is-invalid @enderror" name="expert-algorithm" value="id3" checked required  autofocus>
+                                    <input id="expert-algorithm" type="radio"
+                                           class="form-control @error('expert-algorithm') is-invalid @enderror"
+                                           name="expert_algorithm" value="id3" checked required autofocus>
 
                                     @error('expert-algorithm')
                                     <span class="invalid-feedback" role="alert">
@@ -236,10 +261,12 @@
 
 
                                 <label for="expert-algorithm"
-                                       class="col-sm-2 col-form-label text-md-right">{{ __('BAYES') }}</label>
+                                       class="col-sm-2 col-form-label text-md-right">{{ __('bayes') }}</label>
 
                                 <div class="expert-radio-button">
-                                    <input id="expert-algorithm" type="radio" class="form-control @error('expert-algorithm') is-invalid @enderror" name="expert-algorithm" value="bayes" required  autofocus>
+                                    <input id="expert-algorithm" type="radio"
+                                           class="form-control @error('expert-algorithm') is-invalid @enderror"
+                                           name="expert_algorithm" value="bayes" required autofocus>
 
                                     @error('expert-algorithm')
                                     <span class="invalid-feedback" role="alert">
@@ -249,14 +276,32 @@
                                 </div>
                             </div>
 
-                            <div class="form-group row mb-0">
-                                <div class="col-md-6 offset-md-5">
+                            <div class="md-1 form-group row ">
+                                <div class="col-md-6 offset-md-5 ">
                                     <button type="submit" class="btn btn-primary input-label">
                                         {{ __('Submit Application') }}
                                     </button>
                                 </div>
                             </div>
 
+
+                            @if(isset($result) && $result == 'false')
+                                <div class="expert-row form-group row ">
+                                    <div class="col-md-6 offset-md-4 ">
+                                        <button type="button" class="btn btn-success input-label" disabled>
+                                            Algorithm Result: You Dont't Have a Disease :)
+                                        </button>
+                                    </div>
+                                </div>
+                            @elseif(isset($result) && $result == 'true')
+                                <div class="expert-row form-group row ">
+                                    <div class="col-md-6 offset-md-3 ">
+                                        <button type="button" class="btn btn-danger input-label" disabled>
+                                            Algorithm Result: You Have a Disease :( Please Choose Another Algorithm To Make Sure.
+                                        </button>
+                                    </div>
+                                </div>
+                            @endif
 
                         </form>
                     </div>
