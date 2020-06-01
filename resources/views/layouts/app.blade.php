f3c9aea89b02b529fb2b3c8ea666330b37375273
Nazeeh-Saifi <nazeeh.saifi@gmail.com>
2020-06-01 21:16:58 +0300

    - changing the background image

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -60,7 +60,7 @@
 
             <nav class="nav-menu d-none d-lg-block ">
                 <ul>
-                    <li class="nav-item active"><a class="nav-link"
+                    <li class="nav-item"><a class="nav-link"
                                                    href="{{route('member',Auth::user()->username ?? '')}}">Home</a></li>
                     <li class="nav-item"><a class="nav-link"
                                             href="{{route('member',Auth::user()->username ??'')}}#most_viewed">Most Viewed</a>
@@ -133,67 +133,6 @@
     </header>
     <!-- End Header -->
 
-    {{--
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand d-flex" href="{{ url('/') }}">
-                    <div><img src="/images/s-logo.png" alt="HealthHub logo" style="height: 30px;margin-top: auto;"
-                              class="pr-3"></div>
-                    <div style="margin-top: auto;color:#e9605c;" title="Home Page"><b>H</b><span
-                            style="color: black">ealth</span></div>
-                    <div style="margin-top: auto;color: #e9605c;" title="Home Page"><b>H</b><span
-                            style="color: black">ub</span></div>
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                        aria-controls="navbarSupportedContent" aria-expanded="false"
-                        aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                                   data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                         document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                                          style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    --}}
 
     <main>
         @include('sweetalert::alert')
