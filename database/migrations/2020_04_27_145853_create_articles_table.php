74fd0cfa7cf720b961b429f8acd1d3281b628ea3
Nazeeh-Saifi <nazeeh.saifi@gmail.com>
2020-06-01 14:05:38 +0300

    changing the migration of articles

--- database/migrations/2020_04_27_145853_create_articles_table.php
+++ database/migrations/2020_04_27_145853_create_articles_table.php
@@ -14,7 +14,8 @@ class CreateArticlesTable extends Migration
     public function up()
     {
         Schema::create('articles', function (Blueprint $table) {
-            $table->id('article_id');
+            $table->bigInteger('article_id')->unsigned();
+            $table->primary('article_id');
             $table->timestamps();
             $table->string('title');
             $table->text('description');
