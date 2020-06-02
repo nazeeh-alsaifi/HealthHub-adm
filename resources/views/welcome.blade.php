7a5ba735663991db7050913951ed884ce5e6ed84
Nazeeh-Saifi <nazeeh.saifi@gmail.com>
2020-06-02 23:34:15 +0300

    changing the asset to merge with master

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-<link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
+<link href="{{ asset('css/app.css') }}" rel="stylesheet">
 <div class="loader" style="animation-delay: 4s">
     <svg id="logo" width="556" height="74" viewBox="0 0 556 74" fill="none"
          xmlns="http://www.w3.org/2000/svg">
@@ -34,5 +34,5 @@
     setTimeout(function () {
         window.location.href= 'home'; // the redirect goes here
 
-    },4000); // 5 seconds
+    },7000); // 5 seconds
 </script>
