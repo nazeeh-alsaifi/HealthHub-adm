e9359b0125d1850b4d57cae7b1625a0867e82b58
Nazeeh-Saifi <nazeeh.saifi@gmail.com>
2020-06-02 22:20:32 +0300

    fixing the image in article.show

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -8,7 +8,7 @@
 
                     <div class="card">
                         <div class="card-header col-lg-12 p-3" style="font-family: 'Merriweather', serif;text-align: center"><h2><b>{{$articleInfo->title}}</b></h2></div>
-                        <div class="row "><img src="/public/images/{{$articleInfo->image}}" style="width: 100%; height:80vh;" /></div>
+                        <div><img src="/public/images/{{$articleInfo->image}}" style="width: 100%; height:80vh;" /></div>
                         <div class="row ml-sm-0">
                             <div class="col-lg-12 col-sm-6 mb-2 mt-2" style="font-family: 'Merriweather', serif;text-align: left">
                                 <h5><b>{{$articleInfo->description}}</b></h5>
