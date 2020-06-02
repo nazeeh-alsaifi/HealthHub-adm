bf2e7769062cd728b2a52aab7e8d53deb96fe2eb
Nazeeh-Saifi <nazeeh.saifi@gmail.com>
2020-06-02 15:52:26 +0300

    added some styling for who are we section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -351,14 +351,24 @@
 
         </div>
 
-        <div id="about_us" style="padding: 30px">
-            <div class="row justify-content-center">
-                <div class="py-4 col-md-8">
-                    <div class="card">
-                        <div class="card-header">About Us</div>
+        <div id="about_us" >
+            <section id="partner" class="home-section paddingbot-60">
+                <div class="container marginbot-50">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-8 col-lg-offset-2">
+                            <div class="wow lightSpeedIn" data-wow-delay="0.1s">
+                                <div class="section-heading text-center">
+                                    <h2 class="h-bold">Who Are We..</h2>
+                                    <p>Nazeeh Saifi & Mhd Suliman Tanbari</p>
+                                    <p> students continuing their studies in <a href="https://www.svuonline.org/">Syrian Virtual University</a> in Master Of Web Science.</p>
+                                </div>
+                            </div>
+                            <div class="divider-short"></div>
+                        </div>
                     </div>
                 </div>
-            </div>
+
+            </section>
         </div>
     </div>
 @endsection
