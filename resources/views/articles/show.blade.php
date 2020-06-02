3422485ee0dd3de1efd66cef6433b194792de476
Nazeeh-Saifi <nazeeh.saifi@gmail.com>
2020-06-02 20:10:08 +0300

    changed the article show view

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -1,13 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-    <div id="home" class="container p-5" style="background-image: url('/public/images/{{$articleInfo->image}}')">
+    <div id="home" class="container p-5" ">
         <div id="article" style="padding: 30px">
             <div class="row justify-content-center">
                 <div class="py-4 col-md-8">
 
                     <div class="card">
-                        <div class="card-header col-lg-12 p-3 fa-5x" style="font-family: 'Merriweather', serif;text-align: center"><h1><b>{{$articleInfo->title}}</b></h1></div>
+                        <div class="card-header col-lg-12 p-3" style="font-family: 'Merriweather', serif;text-align: center"><h2><b>{{$articleInfo->title}}</b></h2></div>
+                        <div><img src="/public/images/{{$articleInfo->image}}" style="width: 100%; height:50vh;" /></div>
                         <div class="row">
                             <div class="col-lg-12 col-sm-6 mb-2 mt-2" style="font-family: 'Merriweather', serif;text-align: center">
                                 <h5><b>{{$articleInfo->description}}</b></h5>
