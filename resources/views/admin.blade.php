b2eeb2f8ce7ce475c83e97d58279c390f717e76f
Nazeeh-Saifi <nazeeh.saifi@gmail.com>
2020-06-02 10:05:50 +0300

    - changing the background image - removing the padding to see the changes in heroku

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div id="home" class="container">
-        <div id="most_viewed" style="padding-top: 10vh">
+        <div id="most_viewed" >
             <div class="row justify-content-center">
                 <div class="py-4 col-md-8">
 
@@ -53,7 +53,7 @@
                 </div>
             </div>
         </div>
-        <div id="articles" style="padding: 30px">
+        <div id="articles" >
             <div class="row justify-content-center">
                 <div class="py-4  col-md-8">
 
@@ -108,90 +108,91 @@
                 <!-- /.row -->
             </div>
         </div>
-    </div>
 
-    <div id="users_consultations" style="padding: 30px">
-        <div class="row justify-content-center">
-            <div class="py-4 col-md-8 col-lg-12">
-                <div class="card">
-                    <div class="card-header text-center font-weight-bolder"><span class="card_header_text">Users Consultations</span></div>
-                    <div class="row">
-                        <div class="col-lg-12 col-sm-12 mb-2">
-                            <div class="table-responsive table-wrapper-scroll-y my-custom-scrollbar">
-                                <table class="table table-hover">
-                                    <thead>
-                                    <tr>
-                                        <th scope="col">#</th>
-                                        <th scope="col">Subject</th>
-                                        <th scope="col">User</th>
-                                        <th scope="col">Created At</th>
-                                        <th scope="col"></th>
-                                    </tr>
-                                    </thead>
-                                    <tbody>
-                                    @foreach( $messages as $message)
-                                        <tr class="accordion-toggle collapsed" id="msg{{$message->id}}"
-                                            @if($message->status == 1)style="background-color: #ECECEC" @endif>
-                                            <td class="expand-button" data-toggle="collapse"
-                                                href="#{{'collapse'. $message->id}}"
-                                                onclick="changeStatus({{$message->id}})"></td>
-                                            <td>{{$message->subject}}</td>
-                                            <td>{{ $message->sender->name}}</td>
-                                            <td>{{$message->created_at}}</td>
-                                            @if((Auth::User()->sent->where('reply_on',$message->id)->first()) != null)
-                                                <td><a id="reply-link" data-toggle="collapse"
-                                                       href="#{{'reply'. $message->id}}">View
-                                                        Reply</a></td>
-                                            @else
-                                                <td>
-                                                    <a href="{{url('reply/'.$message->sent_by.'/'.$message->id)}}">Reply</a>
-                                            @endif
+        <div id="users_consultations">
+            <div class="row justify-content-center">
+                <div class="py-4 col-md-8 col-lg-12">
+                    <div class="card">
+                        <div class="card-header text-center font-weight-bolder"><span class="card_header_text">Users Consultations</span>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-12 col-sm-12 mb-2">
+                                <div class="table-responsive table-wrapper-scroll-y my-custom-scrollbar">
+                                    <table class="table table-hover">
+                                        <thead>
+                                        <tr>
+                                            <th scope="col">#</th>
+                                            <th scope="col">Subject</th>
+                                            <th scope="col">User</th>
+                                            <th scope="col">Created At</th>
+                                            <th scope="col"></th>
                                         </tr>
-                                        <tr class="hide-table-padding">
-                                            <td colspan="5">
-                                                <div id="{{'collapse'. $message->id}}" class="collapse in p-3">
-                                                    {{$message->body}}
-                                                </div>
-                                            </td>
-                                        </tr>
-                                        <tr class="hide-table-padding">
-                                            <td colspan="5">
-                                                <div id="{{'reply'. $message->id}}" class="collapse in p-3"
-                                                     style="background-color:#d33e43 ">
-                                                    @if((Auth::User()->sent->where('reply_on',$message->id)->first()) != null)
-                                                        <span style=" font-weight:800; color: white ;">
+                                        </thead>
+                                        <tbody>
+                                        @foreach( $messages as $message)
+                                            <tr class="accordion-toggle collapsed" id="msg{{$message->id}}"
+                                                @if($message->status == 1)style="background-color: #ECECEC" @endif>
+                                                <td class="expand-button" data-toggle="collapse"
+                                                    href="#{{'collapse'. $message->id}}"
+                                                    onclick="changeStatus({{$message->id}})"></td>
+                                                <td>{{$message->subject}}</td>
+                                                <td>{{ $message->sender->name}}</td>
+                                                <td>{{$message->created_at}}</td>
+                                                @if((Auth::User()->sent->where('reply_on',$message->id)->first()) != null)
+                                                    <td><a id="reply-link" data-toggle="collapse"
+                                                           href="#{{'reply'. $message->id}}">View
+                                                            Reply</a></td>
+                                                @else
+                                                    <td>
+                                                        <a href="{{url('reply/'.$message->sent_by.'/'.$message->id)}}">Reply</a>
+                                                @endif
+                                            </tr>
+                                            <tr class="hide-table-padding">
+                                                <td colspan="5">
+                                                    <div id="{{'collapse'. $message->id}}" class="collapse in p-3">
+                                                        {{$message->body}}
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                            <tr class="hide-table-padding">
+                                                <td colspan="5">
+                                                    <div id="{{'reply'. $message->id}}" class="collapse in p-3"
+                                                         style="background-color:#d33e43 ">
+                                                        @if((Auth::User()->sent->where('reply_on',$message->id)->first()) != null)
+                                                            <span style=" font-weight:800; color: white ;">
                                                                Admin :{{Auth::User()->sent->where('reply_on',$message->id)->first()->body}}
                                                         </span>
-                                                    @endif
-                                                </div>
-                                            </td>
-                                        </tr>
-                                    @endforeach
-                                    </tbody>
-                                </table>
+                                                        @endif
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                        </tbody>
+                                    </table>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
 
-    <div id="services" style="padding: 30px">
-        <div class="row justify-content-center">
-            <div class="py-4 col-md-8">
-                <div class="card">
-                    <div class="card-header">services</div>
+        <div id="services" >
+            <div class="row justify-content-center">
+                <div class="py-4 col-md-8">
+                    <div class="card">
+                        <div class="card-header">services</div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
 
-    <div id="about_us" style="padding: 30px">
-        <div class="row justify-content-center">
-            <div class="py-4 col-md-8">
-                <div class="card">
-                    <div class="card-header">About Us</div>
+        <div id="about_us" >
+            <div class="row justify-content-center">
+                <div class="py-4 col-md-8">
+                    <div class="card">
+                        <div class="card-header">About Us</div>
+                    </div>
                 </div>
             </div>
         </div>
